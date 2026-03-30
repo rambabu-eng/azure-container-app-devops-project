b92dc9c0a010a51207609e6460ca45f3ee75d383
Rambabu Katta <rambabu-eng-ram.analytical@gmail.com>
2026-03-31 08:02:37 +1100

    Add updated python file

--- app/src/app.py
+++ app/src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify
+from mssql_python import connect
 import os
-import pymssql
 
 app = Flask(__name__)
 
@@ -10,64 +10,14 @@ def get_db_connection():
     username = os.getenv("SQL_USER")
     password = os.getenv("SQL_PASSWORD")
 
-    return pymssql.connect(
-        server=server,
-        user=username,
-        password=password,
-        database=database,
-        port=1433,
-        timeout=10
+    conn_str = (
+        f"Driver={{ODBC Driver 18 for SQL Server}};"
+        f"Server=tcp:{server},1433;"
+        f"Database={database};"
+        f"Uid={username};"
+        f"Pwd={password};"
+        f"Encrypt=yes;"
+        f"TrustServerCertificate=no;"
+        f"Connection Timeout=30;"
     )
-
-@app.route("/")
-def home():
-    app_name = os.getenv("APP_NAME", "Secure Containerised Web App")
-    environment = os.getenv("ENVIRONMENT", "Development")
-    version = os.getenv("APP_VERSION", "v1")
-    secret = os.getenv("MY_SECRET", "No Secret Found")
-
-    db_status = "Not tested"
-
-    try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT GETDATE()")
-        row = cursor.fetchone()
-        db_status = f"Connected successfully. SQL time: {row[0]}"
-        conn.close()
-    except Exception as e:
-        db_status = f"DB connection failed: {str(e)}"
-
-    return render_template(
-        "index.html",
-        app_name=app_name,
-        environment=environment,
-        version=version,
-        secret=secret,
-        db_status=db_status
-    )
-
-@app.route("/health")
-def health():
-    return jsonify({"status": "healthy"}), 200
-
-@app.route("/dbhealth")
-def dbhealth():
-    try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT GETDATE()")
-        row = cursor.fetchone()
-        conn.close()
-        return jsonify({
-            "status": "connected",
-            "sql_time": str(row[0])
-        }), 200
-    except Exception as e:
-        return jsonify({
-            "status": "failed",
-            "error": str(e)
-        }), 500
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    return connect(conn_str)
